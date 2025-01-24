56bb097bda6a440f6a93e60ba34b3693c954b7cd
Aman <workaman1234@gmail.com>
2025-01-24 17:12:01 +0530

    api endpoints updated

--- api/index.py
+++ api/index.py
@@ -2,9 +2,20 @@ import json
 from http.server import BaseHTTPRequestHandler
 from flask import Flask, jsonify, request
 import json
-import data_source
+
+dictionary=dict()
+
+with open('data.json', 'r') as file:
+    data_list = json.load(file)
+
+data_dict=dict()
+
+for l in data_list:
+    
+    data_dict[l['name']]=l['marks']
 
 class handler(BaseHTTPRequestHandler):
+    
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','application/json')
@@ -12,13 +23,8 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
         return
 
+
 app = Flask(__name__)
-
-dictionary=dict()
-
-
-with open('data.json', 'r') as file:
-    data_dict = json.load(file)
 
 
 @app.route('/api', methods=['GET'])
@@ -26,10 +32,8 @@ def get_data():
 
     X=request.args.getlist('name')
 
-    marks1=data_source.data_dict[X[0]]
-    marks2=data_source.data_dict[X[1]]
-
-
+    marks1=data_dict[X[0]]
+    marks2=data_dict[X[1]]
 
     return jsonify({'marks': [marks1,marks2]})
 
@@ -38,6 +42,8 @@ def home():
 
 
     return jsonify({'location': "index page"})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
